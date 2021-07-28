631775b8ec8de5ad9d764d3dfb6ac8925a42e276
xaveriusjamespalmer <xaveriusjames@itenas.ac.id>
2021-07-28 08:51:06 +0700

    upd_modelfkl

--- application/models/pertanyaanfkl_model.php
+++ application/models/pertanyaanfkl_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Pertanyaanfkl_model extends CI_Model
 {
-    public function getPertanyaanlp2m()
+    public function getPertanyaanfkl()
     {
 
         return $this->db->get('pertanyaan_fkl')->result_array();
