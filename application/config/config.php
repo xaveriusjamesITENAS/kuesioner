9c6c309a2cec139defb812cf6b32d902f2e40efd
Muqiit <mmuqiit.f14@gmail.com>
2022-08-15 13:09:13 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kuesioner/';
+// $config['base_url'] = 'http://localhost/kuesioner/';
 date_default_timezone_set('Asia/Jakarta');
-// $config['base_url'] = 'https://kuesioner.itenas.ac.id/';
+$config['base_url'] = 'https://kuesioner.itenas.ac.id/';
 
 /*
 |--------------------------------------------------------------------------
